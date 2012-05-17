2b85fadd2cfe9a827aac4c74467bffe0c2f3ff4e
Patrick Crosby <patrick@xblabs.com>
2012-05-17 09:37:00 -0500

    switched to get from post, response object

--- lib/stathat.rb
+++ lib/stathat.rb
@@ -1,42 +1,73 @@
 require 'net/http'
 require 'uri'
+require 'json'
 
 module StatHat
         class API
+                CLASSIC_VALUE_URL = "http://api.stathat.com/v"
+                CLASSIC_COUNT_URL = "http://api.stathat.com/c"
+                EZ_URL = "http://api.stathat.com/ez"
+
                 def self.post_value(stat_key, user_key, value)
                         args = { :key => stat_key,
                                 :ukey => user_key,
                                 :value => value }
-                        resp = Net::HTTP.post_form(URI.parse("http://api.stathat.com/v"), args)
-                        return self.response_valid?(resp)
+                        return self.send_to_stathat(CLASSIC_VALUE_URL, args)
                 end
 
                 def self.post_count(stat_key, user_key, count)
                         args = { :key => stat_key,
                                 :ukey => user_key,
-                                :value => value }
-                        resp = Net::HTTP.post_form(URI.parse("http://api.stathat.com/c"), args)
-                        return self.response_valid?(resp)
+                                :value => count }
+                        return self.send_to_stathat(CLASSIC_COUNT_URL, args)
                 end
 
-                def self.ez_post_value(stat_name, account_email, value)
-                        args = { :stat => stat_name, 
-                                :email => account_email, 
+                def self.ez_post_value(stat_name, ezkey, value)
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
                                 :value => value }
-                        resp = Net::HTTP.post_form(URI.parse("http://api.stathat.com/ez"), args)
-                        return self.response_valid?(resp)
+                        return self.send_to_stathat(EZ_URL, args)
                 end
 
-                def self.ez_post_count(stat_name, account_email, count)
-                        args = { :stat => stat_name, 
-                                :email => account_email, 
+                def self.ez_post_count(stat_name, ezkey, count)
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
                                 :count => count }
-                        resp = Net::HTTP.post_form(URI.parse("http://api.stathat.com/ez"), args)
-                        return self.response_valid?(resp)
+                        return self.send_to_stathat(EZ_URL, args)
                 end
 
-                def self.response_valid?(response)
-                        return response.body == "ok"
+                def self.send_to_stathat(url, args)
+                        uri = URI.parse(url)
+                        uri.query = URI.encode_www_form(args)
+                        resp = Net::HTTP.get(uri)
+                        return Response.new(resp)
+                end
+        end
+
+        class Response
+                def initialize(body)
+                        @body = body
+                        @parsed = nil
+                end
+
+                def valid?
+                        return status == 200
+                end
+
+                def status
+                        parse
+                        return @parsed['status']
+                end
+
+                def msg
+                        parse
+                        return @parsed['msg']
+                end
+
+                private
+                def parse
+                        return unless @parsed.nil?
+                        @parsed = JSON.parse(@body)
                 end
         end
 end
--- test/test_stathat.rb
+++ test/test_stathat.rb
@@ -1,7 +1,31 @@
 require 'helper'
 
 class TestStathat < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+        def test_ez_value
+                r = StatHat::API.ez_post_value("test ez value stat", "patrick@stathat.com", 0.92)
+                assert(r.valid?, "response was invalid")
+                assert_equal(r.msg, "ok", "message should be 'ok'")
+                assert_equal(r.status, 200, "status should be 200")
+        end
+
+        def test_ez_count
+                r = StatHat::API.ez_post_value("test ez count stat", "patrick@stathat.com", 12)
+                assert(r.valid?, "response was invalid")
+                assert_equal(r.msg, "ok", "message should be 'ok'")
+                assert_equal(r.status, 200, "status should be 200")
+        end
+
+        def test_classic_count_bad_keys
+                r = StatHat::API.post_count("XXXXXXXX", "YYYYYYYY", 12)
+                assert_equal(r.valid?, false, "response was valid")
+                assert_equal(r.msg, "invalid keys", "incorrect error message")
+                assert_equal(r.status, 500, "incorrect status code")
+        end
+
+        def test_classic_value_bad_keys
+                r = StatHat::API.post_value("ZZZZZZZZ", "YYYYYYYYY", 0.92)
+                assert_equal(r.valid?, false, "response was valid")
+                assert_equal(r.msg, "invalid keys", "incorrect error message")
+                assert_equal(r.status, 500, "incorrect status code")
+        end
 end
