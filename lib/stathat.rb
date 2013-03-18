0f4ff719903830c5577e664d1fb8d5418e6588af
Patrick Crosby <patrick@xblabs.com>
2013-03-17 23:17:34 -0500

    working on threaded version

--- lib/stathat.rb
+++ lib/stathat.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'thread'
 
 module StatHat
         class API
@@ -8,39 +9,93 @@ module StatHat
                 CLASSIC_COUNT_URL = "http://api.stathat.com/c"
                 EZ_URL = "http://api.stathat.com/ez"
 
-                def self.post_value(stat_key, user_key, value)
-                        args = { :key => stat_key,
-                                :ukey => user_key,
-                                :value => value }
-                        return self.send_to_stathat(CLASSIC_VALUE_URL, args)
+                REPORTER = self.new()
+
+                def initialize()
+                        @Q = Queue.new
+                        run_pool()
                 end
 
-                def self.post_count(stat_key, user_key, count)
-                        args = { :key => stat_key,
-                                :ukey => user_key,
-                                :count => count }
-                        return self.send_to_stathat(CLASSIC_COUNT_URL, args)
+                def enqueue(url, args)
+                        point = {:url => url, :args => args}
+                        @Q << point
                 end
 
-                def self.ez_post_value(stat_name, ezkey, value)
-                        args = { :stat => stat_name,
-                                :ezkey => ezkey,
-                                :value => value }
-                        return self.send_to_stathat(EZ_URL, args)
+                def run_pool()
+                        @running = true
+                        @pool = []
+                        5.times do |i|
+                                pool[i] = Thread.new do
+                                        while @running do
+                                                point = @Q.pop
+                                                # XXX check for error?
+                                                send_to_stathat(point[:url], point[:args])
+                                        end
+                                end
+                        end
                 end
 
-                def self.ez_post_count(stat_name, ezkey, count)
-                        args = { :stat => stat_name,
-                                :ezkey => ezkey,
-                                :count => count }
-                        return self.send_to_stathat(EZ_URL, args)
+                def stop_pool()
+                        @running = false
+                        @pool.each do |th|
+                                th.join
+                        end
                 end
 
-                def self.send_to_stathat(url, args)
+                def finish()
+                        stop_pool
+                        # XXX serialize queue?
+                end
+
+                def send_to_stathat(url, args)
                         uri = URI.parse(url)
                         uri.query = URI.encode_www_form(args)
                         resp = Net::HTTP.get(uri)
                         return Response.new(resp)
+                end
+
+                def post_value(stat_key, user_key, value)
+                        args = { :key => stat_key,
+                                :ukey => user_key,
+                                :value => value }
+                        enqueue(CLASSIC_VALUE_URL, args)
+                end
+
+                def self.post_value(stat_key, user_key, value)
+                        REPORTER.post_value(stat_key, user_key, value)
+                end
+
+                def post_count(stat_key, user_key, count)
+                        args = { :key => stat_key,
+                                :ukey => user_key,
+                                :count => count }
+                        enqueue(CLASSIC_COUNT_URL, args)
+                end
+
+                def self.post_count(stat_key, user_key, count)
+                        REPORTER.post_count(stat_key, user_key, count)
+                end
+
+                def ez_post_value(stat_name, ezkey, value)
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
+                                :value => value }
+                        enqueue(EZ_URL, args)
+                end
+
+                def self.ez_post_value(stat_name, ezkey, value)
+                        REPORTER.ez_post_value(stat_name, ezkey, value)
+                end
+
+                def ez_post_count(stat_name, ezkey, count)
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
+                                :count => count }
+                        enqueue(EZ_URL, args)
+                end
+
+                def self.ez_post_count(stat_name, ezkey, count)
+                        REPORTER.ez_post_count(stat_name, ezkey, count)
                 end
         end
 
