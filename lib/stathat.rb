42d9bf06db0434868a3ed1f608690f21f7b59b4b
Patrick Crosby <patrick@xblabs.com>
2013-03-19 14:00:38 -0500

    added mutex around @running

--- lib/stathat.rb
+++ lib/stathat.rb
@@ -2,44 +2,41 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'thread'
+require 'singleton'
 
 module StatHat
         class API
+                class << self
+                        def ez_post_value(stat_name, ezkey, value, &block)
+                                Reporter.instance.ez_post_value(stat_name, ezkey, value, block)
+                        end
+
+                        def ez_post_count(stat_name, ezkey, count, &block)
+                                Reporter.instance.ez_post_count(stat_name, ezkey, count, block)
+                        end
+
+                        def post_count(stat_key, user_key, count, &block)
+                                Reporter.instance.post_count(stat_key, user_key, count, block)
+                        end
+
+                        def post_value(stat_key, user_key, value, &block)
+                                Reporter.instance.post_value(stat_key, user_key, value, block)
+                        end
+                end
+        end
+
+        class Reporter
+                include Singleton
+
                 CLASSIC_VALUE_URL = "http://api.stathat.com/v"
                 CLASSIC_COUNT_URL = "http://api.stathat.com/c"
                 EZ_URL = "http://api.stathat.com/ez"
 
-                REPORTER = self.new()
 
-                def initialize()
-                        @Q = Queue.new
+                def initialize
+                        @que = Queue.new
+                        @runlock = Mutex.new
                         run_pool()
-                end
-
-                def enqueue(url, args)
-                        point = {:url => url, :args => args}
-                        @Q << point
-                end
-
-                def run_pool()
-                        @running = true
-                        @pool = []
-                        5.times do |i|
-                                pool[i] = Thread.new do
-                                        while @running do
-                                                point = @Q.pop
-                                                # XXX check for error?
-                                                send_to_stathat(point[:url], point[:args])
-                                        end
-                                end
-                        end
-                end
-
-                def stop_pool()
-                        @running = false
-                        @pool.each do |th|
-                                th.join
-                        end
                 end
 
                 def finish()
@@ -47,55 +44,84 @@ module StatHat
                         # XXX serialize queue?
                 end
 
+                def post_value(stat_key, user_key, value, cb)
+                        args = { :key => stat_key,
+                                :ukey => user_key,
+                                :value => value }
+                        enqueue(CLASSIC_VALUE_URL, args, cb)
+                end
+
+                def post_count(stat_key, user_key, count, cb)
+                        args = { :key => stat_key,
+                                :ukey => user_key,
+                                :count => count }
+                        enqueue(CLASSIC_COUNT_URL, args, cb)
+                end
+
+                def ez_post_value(stat_name, ezkey, value, cb)
+                        puts "ezval cb: #{cb}"
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
+                                :value => value }
+                        enqueue(EZ_URL, args, cb)
+                end
+
+                def ez_post_count(stat_name, ezkey, count, cb)
+                        args = { :stat => stat_name,
+                                :ezkey => ezkey,
+                                :count => count }
+                        enqueue(EZ_URL, args, cb)
+                end
+
+                private
+                def run_pool
+                        @runlock.synchronize { @running = true }
+                        @pool = []
+                        5.times do |i|
+                                @pool[i] = Thread.new do
+                                        puts "thread #{i} started"
+                                        while true do
+                                                point = @que.pop
+                                                # XXX check for error?
+                                                begin
+                                                        puts "thread #{i}: sending"
+                                                        resp = send_to_stathat(point[:url], point[:args])
+                                                        if point[:cb]
+                                                                point[:cb].call(resp)
+                                                        end
+                                                rescue
+                                                        pp $!
+                                                end
+                                                @runlock.synchronize {
+                                                        break unless @running
+                                                }
+                                        end
+                                        puts "reporter thread #{i} finished"
+                                end
+                        end
+                end
+
+                def stop_pool()
+                        @runlock.synchronize {
+                                @running = false
+                        }
+                        @pool.each do |th|
+                                th.join if th && th.alive?
+                        end
+                end
+
+                def enqueue(url, args, cb=nil)
+                        return false unless @running
+                        point = {:url => url, :args => args, :cb => cb}
+                        @que << point
+                        true
+                end
+
                 def send_to_stathat(url, args)
                         uri = URI.parse(url)
                         uri.query = URI.encode_www_form(args)
                         resp = Net::HTTP.get(uri)
                         return Response.new(resp)
-                end
-
-                def post_value(stat_key, user_key, value)
-                        args = { :key => stat_key,
-                                :ukey => user_key,
-                                :value => value }
-                        enqueue(CLASSIC_VALUE_URL, args)
-                end
-
-                def self.post_value(stat_key, user_key, value)
-                        REPORTER.post_value(stat_key, user_key, value)
-                end
-
-                def post_count(stat_key, user_key, count)
-                        args = { :key => stat_key,
-                                :ukey => user_key,
-                                :count => count }
-                        enqueue(CLASSIC_COUNT_URL, args)
-                end
-
-                def self.post_count(stat_key, user_key, count)
-                        REPORTER.post_count(stat_key, user_key, count)
-                end
-
-                def ez_post_value(stat_name, ezkey, value)
-                        args = { :stat => stat_name,
-                                :ezkey => ezkey,
-                                :value => value }
-                        enqueue(EZ_URL, args)
-                end
-
-                def self.ez_post_value(stat_name, ezkey, value)
-                        REPORTER.ez_post_value(stat_name, ezkey, value)
-                end
-
-                def ez_post_count(stat_name, ezkey, count)
-                        args = { :stat => stat_name,
-                                :ezkey => ezkey,
-                                :count => count }
-                        enqueue(EZ_URL, args)
-                end
-
-                def self.ez_post_count(stat_name, ezkey, count)
-                        REPORTER.ez_post_count(stat_name, ezkey, count)
                 end
         end
 
--- test/test_stathat.rb
+++ test/test_stathat.rb
@@ -1,31 +1,41 @@
 require 'helper'
 
+
 class TestStathat < MiniTest::Unit::TestCase
+
         def test_ez_value
-                r = StatHat::API.ez_post_value("test ez value stat", "patrick@stathat.com", 0.92)
-                assert(r.valid?, "response was invalid")
-                assert_equal(r.msg, "ok", "message should be 'ok'")
-                assert_equal(r.status, 200, "status should be 200")
+                StatHat::API.ez_post_value("test ez value stat", "test@stathat.com", 0.92) do |resp|
+                        assert(resp.valid?, "response was invalid")
+                        assert_equal(resp.msg, "ok", "message should be 'ok'")
+                        assert_equal(resp.status, 200, "status should be 200")
+                end
+                sleep(1)
         end
 
         def test_ez_count
-                r = StatHat::API.ez_post_value("test ez count stat", "patrick@stathat.com", 12)
-                assert(r.valid?, "response was invalid")
-                assert_equal(r.msg, "ok", "message should be 'ok'")
-                assert_equal(r.status, 200, "status should be 200")
+                StatHat::API.ez_post_value("test ez count stat", "test@stathat.com", 12) do |r|
+                        assert(r.valid?, "response was invalid")
+                        assert_equal(r.msg, "ok", "message should be 'ok'")
+                        assert_equal(r.status, 200, "status should be 200")
+                end
+                sleep(1)
         end
 
         def test_classic_count_bad_keys
-                r = StatHat::API.post_count("XXXXXXXX", "YYYYYYYY", 12)
-                assert_equal(r.valid?, false, "response was valid")
-                assert_equal(r.msg, "invalid keys", "incorrect error message")
-                assert_equal(r.status, 500, "incorrect status code")
+                StatHat::API.post_count("XXXXXXXX", "YYYYYYYY", 12) do |r|
+                        assert_equal(r.valid?, false, "response was valid")
+                        assert_equal(r.msg, "invalid keys", "incorrect error message")
+                        assert_equal(r.status, 500, "incorrect status code")
+                end
+                sleep(1)
         end
 
         def test_classic_value_bad_keys
-                r = StatHat::API.post_value("ZZZZZZZZ", "YYYYYYYYY", 0.92)
-                assert_equal(r.valid?, false, "response was valid")
-                assert_equal(r.msg, "invalid keys", "incorrect error message")
-                assert_equal(r.status, 500, "incorrect status code")
+                StatHat::API.post_value("ZZZZZZZZ", "YYYYYYYYY", 0.92) do |r|
+                        assert_equal(r.valid?, false, "response was valid")
+                        assert_equal(r.msg, "invalid keys", "incorrect error message")
+                        assert_equal(r.status, 500, "incorrect status code")
+                end
+                sleep(1)
         end
 end
